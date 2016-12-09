972df60d6c1bcba64d8df15fe18a76ee3cdf2d1d
meerajahmed <meerajahmed@gmail.com>
2016-12-09 16:27:56 +0530

    show ad in free version of app

--- app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivityFragment.java
@@ -1,4 +1,5 @@
-package com.udacity.gradle.builditbigger;
+package com.udacity.gradle.builditbigger.free;
+
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +10,12 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.android.libjokeandroid.JokeActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.udacity.gradle.builditbigger.JokeAsyncTask;
+import com.udacity.gradle.builditbigger.R;
+
+import static java.security.AccessController.getContext;
 
 
 /**
@@ -16,7 +23,9 @@ import com.example.android.libjokeandroid.JokeActivity;
  */
 public class MainActivityFragment extends Fragment {
 
+
     public MainActivityFragment() {
+        // Required empty public constructor
     }
 
     @Override
@@ -28,7 +37,7 @@ public class MainActivityFragment extends Fragment {
 
             @Override
             public void onClick(View view) {
-                new JokeAsyncTask( new JokeAsyncTask.JokeListener(){
+                new JokeAsyncTask(new JokeAsyncTask.JokeListener(){
                     @Override
                     public void onJokeLoad(String joke) {
                         Intent intent = new Intent(getContext(), JokeActivity.class);
@@ -38,7 +47,17 @@ public class MainActivityFragment extends Fragment {
                 }).execute();
             }
         });
+
+        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
         return root;
     }
+
 
 }
